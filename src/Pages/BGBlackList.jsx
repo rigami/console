8d1e56b2b0d486d70e70411a0d2ea7cf409edc4a
Danilkinkin <danilkinkin@gmail.com>
2021-01-23 00:57:28 +0400

    Added examples

--- src/Pages/BGBlackList.jsx
+++ src/Pages/BGBlackList.jsx
@@ -18,6 +18,14 @@ import {
     Toolbar,
     SaveButton,
 } from 'react-admin';
+import { Divider, Typography, Box } from "@material-ui/core";
+import {makeStyles} from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+    margin: {
+        margin: theme.spacing(2, 0),
+    },
+}));
 
 const PostFilter = (props) => (
     <Filter {...props}>
@@ -67,7 +75,7 @@ const ListActions = (props) => {
 export const BGBlackListList = (props) => (
     <List {...props} exporter={false} actions={<ListActions />} filters={<PostFilter />}>
         <Datagrid>
-            <TextField source="bgId" label="Id in service" />
+            <TextField source="entityId" label="Id in service" />
             <TextField source="service" />
             <TextField source="entityType" />
             <UrlField source="sourceLink" target="_blank" />
@@ -92,21 +100,62 @@ const PostCreateToolbar = props => (
     </Toolbar>
 );
 
+function ExampleCreateRow() {
+    const classes = useStyles();
 
-export const BGBlackListCreate = (props) => (
-    <Create {...props} title="Add background to black list" actions={<PostEditActions />}>
-        <SimpleForm toolbar={<PostCreateToolbar />}>
-            <TextInput source="bgId" label="Id in service" />
-            <TextInput source="sourceLink" />
-            <SelectInput source="service" allowEmpty={false} choices={[
-                { id: 'UNSPLASH', name: 'Unsplash' },
-                { id: 'PEXELS', name: 'Pexels' },
-                { id: 'PIXABAY', name: 'Pixabay' },
-            ]} />
-            <SelectInput source="entityType" allowEmpty={false} choices={[
-                { id: 'PUBLISHER', name: 'Publisher' },
-                { id: 'BACKGROUND', name: 'Background' },
-            ]} />
-        </SimpleForm>
-    </Create>
-);
+    return (
+        <Box>
+            <Typography variant="h5">Examples</Typography>
+            <Typography variant="h6">Publisher</Typography>
+            <Typography>
+                <Typography variant="span" color="textSecondary">Unsplash: </Typography>
+                https://unsplash.com/<Typography variant="span" color="secondary">@danilkinkin</Typography>
+            </Typography>
+            <Typography>
+                <Typography variant="span" color="textSecondary">Pexels: </Typography>
+                https://www.pexels.com/<Typography variant="span" color="secondary">@danil-zakhvatkin-11197472</Typography>/
+            </Typography>
+            <Typography>
+                <Typography variant="span" color="textSecondary">Pixabay: </Typography>
+                https://pixabay.com/ru/users/<Typography variant="span" color="secondary">danilkinkin</Typography>-5843520/
+            </Typography>
+            <Typography variant="h6">Background</Typography>
+            <Typography>
+                <Typography variant="span" color="textSecondary">Unsplash: </Typography>
+                https://unsplash.com/photos/<Typography variant="span" color="secondary">nL1pAWmRFYU</Typography>
+            </Typography>
+            <Typography>
+                <Typography variant="span" color="textSecondary">Pexels: </Typography>
+                https://www.pexels.com/video/silhouette-of-maple-leaves-<Typography variant="span" color="secondary">1510090</Typography>/
+            </Typography>
+            <Typography>
+                <Typography variant="span" color="textSecondary">Pixabay: </Typography>
+                https://pixabay.com/videos/nature-rain-plant-water-garden-<Typography variant="span" color="secondary">42420</Typography>/
+            </Typography>
+            <Divider className={classes.margin} />
+        </Box>
+    );
+}
+
+
+export function BGBlackListCreate (props) {
+    return (
+        <Create {...props} title="Add background to black list" actions={<PostEditActions />}>
+            <SimpleForm toolbar={<PostCreateToolbar />}>
+                <ExampleCreateRow />
+                <Typography variant="h5">Add row</Typography>
+                <TextInput source="entityId" label="Id in service" />
+                <TextInput source="sourceLink" />
+                <SelectInput source="service" allowEmpty={false} choices={[
+                    { id: 'UNSPLASH', name: 'Unsplash' },
+                    { id: 'PEXELS', name: 'Pexels' },
+                    { id: 'PIXABAY', name: 'Pixabay' },
+                ]} />
+                <SelectInput source="entityType" allowEmpty={false} choices={[
+                    { id: 'PUBLISHER', name: 'Publisher' },
+                    { id: 'BACKGROUND', name: 'Background' },
+                ]} />
+            </SimpleForm>
+        </Create>
+    );
+}
