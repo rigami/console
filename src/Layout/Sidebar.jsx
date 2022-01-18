8911d25d47e63f8b08545a79a5d0178698891e68
danilkinkin <danilkinkin@gmail.com>
2022-01-18 23:10:32 +0400

    [SideBar] Fixed width

--- src/Layout/Sidebar.jsx
+++ src/Layout/Sidebar.jsx
@@ -1,10 +1,28 @@
 import { Sidebar } from "react-admin";
+import {makeStyles} from "@material-ui/core/styles";
 
-const CustomSidebar = (props) => (
-    <Sidebar
-        {...props}
-        userMenu={false}
-    />
-);
+const useStyles = makeStyles(theme => ({
+    root: { height: 'unset' },
+    drawerPaper: { width: 200 },
+    fixed: { height: 'unset', width: 'inherit', },
+}));
+
+const CustomSidebar = ({ children, ...props }) => {
+    const classes = useStyles();
+
+    return (
+        <Sidebar
+            {...props}
+            classes={{
+                root: classes.root,
+                drawerPaper: classes.drawerPaper,
+                fixed: classes.fixed,
+            }}
+            userMenu={false}
+        >
+            {children}
+        </Sidebar>
+    );
+};
 
 export default CustomSidebar;
